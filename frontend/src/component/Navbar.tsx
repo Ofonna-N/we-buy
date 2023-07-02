7cf6d191fce3e3fa70e0e4c73353aa3c9d049666
Ofonna-N <nwezeofonna@gmail.com>
2023-07-02 11:30:35 -0500

    Refatored nav bar scripts and added accebilty features

--- frontend/src/component/Navbar.tsx
+++ frontend/src/component/Navbar.tsx
@@ -8,6 +8,7 @@ import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import Drawer from "@mui/material/Drawer";
 import {
+  Container,
   IconButton,
   List,
   ListItem,
@@ -40,58 +41,99 @@ const Navbar = () => {
     setToggled((prev) => !prev);
   };
 
+  const sideMenuNavItems = [
+    <ListItemButton>
+      <ListItemIcon>
+        <ShoppingCartIcon />
+      </ListItemIcon>
+      <ListItemText primary="Cart" />
+    </ListItemButton>,
+    <ListItemButton>
+      <ListItemIcon>
+        <PersonIcon />
+      </ListItemIcon>
+      <ListItemText primary="Sign In" />
+    </ListItemButton>,
+    <ListItemButton disableRipple>
+      <ListItemText>
+        <FormControlLabel
+          control={<Switch size="small" />}
+          label={
+            <Typography
+              color={"rgba(255, 255, 255, 0.5)"}
+              ml={"1.65rem"}
+              aria-label="dark mode"
+            >
+              Dark
+            </Typography>
+          }
+          labelPlacement="end"
+          onChange={() => darkModeCtx.toggleMode()}
+          checked={darkModeCtx.mode === "dark"}
+        />
+      </ListItemText>
+    </ListItemButton>,
+  ];
+
   return (
     <>
       <AppBar position="relative">
-        <Toolbar component={"nav"}>
-          <IconButton
-            size="large"
-            sx={{
-              display: { xs: "block", sm: "none" },
-              color: "white",
-            }}
-            aria-label="menu button"
-            onClick={handleDrawerToggle}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography
-            variant="h4"
-            component={"span"}
-            aria-label="logo"
-            sx={{
-              flexGrow: "1",
-              textAlign: { xs: "center", sm: "left" },
-            }}
-          >
-            We Buy
-          </Typography>
-          <Box display={{ xs: "none", sm: "flex" }} gap={"1rem"}>
-            <LinkBtn href="#" startIcon={<ShoppingCartIcon />}>
-              Cart
-            </LinkBtn>
-            <LinkBtn href="#" startIcon={<PersonIcon />}>
-              Sign In
-            </LinkBtn>
-          </Box>
-          <FormControlLabel
-            control={<Switch size="small" />}
-            label={
-              <Typography
-                color={"rgba(255, 255, 255, 0.5)"}
-                fontSize={"0.75rem"}
-              >
-                Dark
-              </Typography>
-            }
-            labelPlacement="start"
-            onChange={() => darkModeCtx.toggleMode()}
-            checked={darkModeCtx.mode === "dark"}
-            sx={{
-              display: { xs: "none", sm: "flex" },
-            }}
-          />
-        </Toolbar>
+        <Container fixed disableGutters>
+          <Toolbar component={"nav"}>
+            <IconButton
+              size="large"
+              sx={{
+                display: { xs: "block", sm: "none" },
+                color: "white",
+              }}
+              aria-label="Toggle Menu"
+              aria-expanded={toggled}
+              role="button"
+              aria-haspopup="menu"
+              onClick={handleDrawerToggle}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography
+              variant="h4"
+              component={"span"}
+              aria-label="logo"
+              role=""
+              sx={{
+                flexGrow: "1",
+                textAlign: { xs: "center", sm: "left" },
+              }}
+            >
+              We Buy
+            </Typography>
+            <Box display={{ xs: "none", sm: "flex" }} gap={"1rem"}>
+              <LinkBtn href="#" startIcon={<ShoppingCartIcon />}>
+                Cart
+              </LinkBtn>
+              <LinkBtn href="#" startIcon={<PersonIcon />}>
+                Sign In
+              </LinkBtn>
+            </Box>
+            <FormControlLabel
+              control={<Switch size="small" />}
+              label={
+                <Typography
+                  color={"rgba(255, 255, 255, 0.5)"}
+                  fontSize={"0.75rem"}
+                  aria-label="dark mode"
+                >
+                  Dark
+                </Typography>
+              }
+              labelPlacement="start"
+              onChange={() => darkModeCtx.toggleMode()}
+              checked={darkModeCtx.mode === "dark"}
+              sx={{
+                display: { xs: "none", sm: "flex" },
+              }}
+            />
+          </Toolbar>
+        </Container>
       </AppBar>
       <Drawer
         open={toggled}
@@ -120,44 +162,12 @@ const Navbar = () => {
         }}
         onClose={handleDrawerToggle}
         sx={{ display: { xs: "block", sm: "none" } }}
+        role="menu"
       >
         <List>
-          <ListItem>
-            <ListItemButton>
-              <ListItemIcon>
-                <ShoppingCartIcon />
-              </ListItemIcon>
-              <ListItemText primary="Cart" />
-            </ListItemButton>
-          </ListItem>
-          <ListItem>
-            <ListItemButton>
-              <ListItemIcon>
-                <PersonIcon />
-              </ListItemIcon>
-              <ListItemText primary="Sign In" />
-            </ListItemButton>
-          </ListItem>
-          <ListItem>
-            <ListItemButton disableRipple>
-              <ListItemText>
-                <FormControlLabel
-                  control={<Switch size="small" />}
-                  label={
-                    <Typography
-                      color={"rgba(255, 255, 255, 0.5)"}
-                      ml={"1.65rem"}
-                    >
-                      Dark
-                    </Typography>
-                  }
-                  labelPlacement="end"
-                  onChange={() => darkModeCtx.toggleMode()}
-                  checked={darkModeCtx.mode === "dark"}
-                />
-              </ListItemText>
-            </ListItemButton>
-          </ListItem>
+          {sideMenuNavItems.map((item, i) => (
+            <ListItem key={i}>{item}</ListItem>
+          ))}
         </List>
       </Drawer>
     </>
