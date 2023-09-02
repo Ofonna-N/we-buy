02ce826330f8d0d803bd60277273a3fb491cb066
Ofonna-N <nwezeofonna@gmail.com>
2023-09-02 14:06:58 -0500

    refactored redirection logic to sign in page not sign in form

--- frontend/src/pages/signIn/SignInPage.tsx
+++ frontend/src/pages/signIn/SignInPage.tsx
@@ -2,8 +2,22 @@ import { Box, Typography } from "@mui/material";
 import AppContainer from "../../component/page/AppContainer";
 
 import SignInForm from "./components/SignInForm";
+import { useMemo, useEffect } from "react";
+import { useNavigate, useLocation } from "react-router-dom";
+import RoutesPaths from "../../constants/RoutePaths";
 
 const SignInPage = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const params = useMemo(() => {
+    return new URLSearchParams(location.search);
+  }, [location]);
+
+  const redirectParam = params.get("redirect");
+
+  useEffect(() => {
+    if (redirectParam) navigate(RoutesPaths.HOME_ROUTE);
+  }, [redirectParam]);
   return (
     <AppContainer>
       <Box marginBottom={"1.5rem"}>
