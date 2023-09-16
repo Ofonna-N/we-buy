afb0672ff12ee7625299f4f7ab7ece456627aa2b
Ofonna-N <nwezeofonna@gmail.com>
2023-09-16 09:32:57 -0500

    removed completed TODO comment

--- frontend/src/hooks/api-hooks/orders/useMutatePlaceOrder.ts
+++ frontend/src/hooks/api-hooks/orders/useMutatePlaceOrder.ts
@@ -6,7 +6,6 @@ const useMutatePlaceOrder = () => {
     console.log("order placed: ", data);
   };
 
-  // TODO: Refactor all endpoints into a constant
   return useMutateData(endpointRoutes.ORDERS.ORDERS, onOrderPlaced);
 };
 
