6852cd9f595faf28faea4a55417ac1ff78c6866e
Ofonna-N <nwezeofonna@gmail.com>
2023-10-30 00:04:22 -0500

    update vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,13 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+const isProduction = process.env.NODE_ENV === "production";
+const apiTarget = isProduction
+  ? "https://we-buy.onrender.com"
+  : "http://localhost:4000";
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:4000",
+        target: apiTarget,
         changeOrigin: true,
       },
     },
