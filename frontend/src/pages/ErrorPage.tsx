8162cfaff00e0bf049eb44c9ae9fb9682ceb2299
Ofonna-N <nwezeofonna@gmail.com>
2023-10-30 00:35:12 -0500

    deployment setup v8

--- frontend/src/pages/ErrorPage.tsx
+++ frontend/src/pages/ErrorPage.tsx
@@ -1,24 +1,25 @@
 import { Box, Paper, Typography, Divider } from "@mui/material";
-import { useRouteError, isRouteErrorResponse } from "react-router-dom";
+// import { useRouteError, isRouteErrorResponse } from "react-router-dom";
 import AppContainer from "../component/page/AppContainer";
 
 const ErrorPage = () => {
-  const error = useRouteError();
+  // const error = useRouteError();
 
   return (
     <Box>
       <AppContainer>
         <Paper variant="elevation" elevation={3} sx={{ padding: "1rem" }}>
           <Typography variant="h3">
-            {isRouteErrorResponse(error)
+            {/* {isRouteErrorResponse(error)
               ? "Page Unavailable"
-              : "Unexpected Error"}
+              : "Unexpected Error"} */}
           </Typography>
           <Divider />
           <Typography variant="body1" paddingTop={"1rem"}>
-            {isRouteErrorResponse(error)
+            {/* {isRouteErrorResponse(error)
               ? error.error?.message
-              : "Something went wrong"}
+              : "Something went wrong"} */}
+            Page Unavailable
           </Typography>
         </Paper>
       </AppContainer>
