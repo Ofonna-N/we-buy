148c72044dd326a76e441608873bf483836d17ec
Ofonna-N <nwezeofonna@gmail.com>
2023-10-29 22:22:02 -0500

    Added setup up for deployment

--- backend/middleware/appDefaultMiddleware.js
+++ backend/middleware/appDefaultMiddleware.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const router = express.Router();
+const path = require("path");
 
 router.use(express.json());
 router.use(express.urlencoded({ extended: true }));
 router.use(cookieParser(process.env.AUTHCOOKIE_SECRET));
 
+if (process.env.NODE_ENV === "production") {
+  router.use(express.static(path.join(__dirname, "../../frontend/dist")));
+}
 module.exports = router;
--- backend/routes/homeRoute.js
+++ backend/routes/homeRoute.js
@@ -1,8 +1,16 @@
 const express = require("express");
 const router = express.Router();
+const path = require("path");
 
-router.get("/", (_, res) => {
-  return res.send("Welcome to we by Api");
-});
+if (process.env.NODE_ENV === "production") {
+  router.get("/", (_, res) => {
+    return res.sendFile(path.join(__dirname, "../../frontend/dist/index.html"));
+  });
+} else {
+  // console.log(path);
+  router.get("/", (_, res) => {
+    return res.send("Welcome to we by Api");
+  });
+}
 
 module.exports = router;
